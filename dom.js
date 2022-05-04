eda02594375a76a813277aa290c359bd7f21b710
Prateekkumar-repository <96733993+Prateekkumar-repository@users.noreply.github.com>
2022-05-04 19:00:04 +0530

    Items text 3 made green and all items made bold

--- dom.js
+++ dom.js
@@ -1,4 +1,12 @@
-const heading1 = document.querySelector('#header-title');
-const heading2 = document.querySelector('#main-header');
-heading1.innerHTML = "Title got changed";
-heading2.style.borderBottom = 'solid 2px black';
+// const heading1 = document.querySelector('#header-title');
+// const heading2 = document.querySelector('#main-header');
+// heading1.innerHTML = "Title got changed";
+// heading2.style.borderBottom = 'solid 2px black';
+
+
+const title = document.getElementsByClassName('list-group-item');
+console.log(title[2].style.color='green');
+console.log(title[0].style.fontWeight='bold');
+console.log(title[1].style.fontWeight='bold');
+console.log(title[2].style.fontWeight='bold');
+console.log(title[3].style.fontWeight='bold');
