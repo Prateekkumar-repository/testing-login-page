ed612a15689b4db3f4dc6de8a5dcfdbb0201977f
Prateekkumar-repository <96733993+Prateekkumar-repository@users.noreply.github.com>
2022-05-04 14:11:29 +0530

    registration page

---
+++ script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('#btn');
+const check = document.querySelector('#check');
+
+button.addEventListener('click', function() {  
+
+    console.log('Submitted', event);
+
+
+})
