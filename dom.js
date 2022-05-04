aabb03c62b347789174c6608a143c0b452cc3503
Prateekkumar-repository <96733993+Prateekkumar-repository@users.noreply.github.com>
2022-05-04 19:25:38 +0530

    add li and get by id and class

--- dom.js
+++ dom.js
@@ -1,12 +1,19 @@
-// const heading1 = document.querySelector('#header-title');
-// const heading2 = document.querySelector('#main-header');
-// heading1.innerHTML = "Title got changed";
-// heading2.style.borderBottom = 'solid 2px black';
+// by id
+// const title = document.getElementsByTagName('li');
 
+// title[2].style.backgroundColor ='green';
+
+// for(let i=0; i<title.length; i++) {
+//     title[i].style.fontWeight ='bold';
+    
+// }
+
+// bs class 
 
 const title = document.getElementsByClassName('list-group-item');
-console.log(title[2].style.color='green');
-console.log(title[0].style.fontWeight='bold');
-console.log(title[1].style.fontWeight='bold');
-console.log(title[2].style.fontWeight='bold');
-console.log(title[3].style.fontWeight='bold');
+title[2].style.backgroundColor ='green';
+
+for(let i=0; i<title.length; i++) {
+    title[i].style.fontWeight ='bold';
+    
+}
